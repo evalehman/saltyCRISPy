dfa0c369c8f2a099f362508d6956b80aac38ac49
evalehman <evamlehman@gmail.com>
2026-03-01 20:59:41 -0500

    Add files via upload

---
+++ downloadAndDefense.sh
@@ -0,0 +1,29 @@
+# Script used to download multiple genomes and run defense finder on said genomes
+# Input: path to tsv file, column containing accession numbers, directory
+# Usage example: ./downloadAndDF.sh imgInput/img_freshwater_test.tsv 3 freshGenomes
+# Before running: conda activate defense-finder
+
+INPUT_PATH=$1
+COL=$2
+
+DATA=$(cut -f $COL $INPUT_PATH | tail -n +2)
+cd $3
+for acc in $DATA; do
+	# Set-up
+	mkdir $acc
+	cd $acc
+
+	# Download
+	echo Downloading $acc.fasta
+	esearch -db assembly -query $acc | elink -target nuccore | efetch -format fasta > $acc.fasta
+	echo Downloaded $acc.fasta successfully
+
+	# Defense Finder
+	echo Running defense finder on $acc.fasta
+	defense-finder run $acc.fasta --out-dir dfOutput
+	echo Ran defense finder on $acc.fasta successfully
+
+	# Clean up
+	cd ..
+done
+
