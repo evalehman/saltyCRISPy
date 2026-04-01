120c4f1d9cfc0b06b62695960fdbe2fba8931ad8
evalehman <evamlehman@gmail.com>
2026-04-01 13:37:52 -0400

    Add files via upload

---
+++ table-maker.sh
@@ -0,0 +1,28 @@
+# Usage example: ./table-maker.sh DIR1 DIR2 (ect.)
+
+# Create file and headers
+echo "Accession Number,Salinity Prediction,Cas Presence,Dataset" > data_table.csv
+
+for arg in "$@"; do
+    DATASET=$arg
+
+    # Iterate through subdirectories
+    for acc in "${DATASET}"/*/; do
+    
+        # Get accession/subdirectory names
+        ACCESSION=$(basename "$acc") 
+        
+        # Get normalized mean from normmean.txt under predictions directory
+        NMEAN=$(grep $ACCESSION predictions_$DATASET/normmean.txt | cut -d' ' -f2) 
+        
+        # Get crispr +/- from casresults.txt
+        if grep "$ACCESSION" "$DATASET"/casresults.txt | grep "NONE"; then
+            CAS="NO"
+        else
+            CAS="YES"
+        fi
+        
+        # Write to the file
+        echo "$ACCESSION,$NMEAN,$CAS,$DATASET" >> data_table.csv
+    done
+done
