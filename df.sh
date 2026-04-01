5a8ffb16ca40153c50284a52ac2a7ef2d8e48d16
evalehman <evamlehman@gmail.com>
2026-04-01 13:40:12 -0400

    Add files via upload

---
+++ df.sh
@@ -0,0 +1,16 @@
+# Run DefenseFinder on a directory full of genomes or proteomes 
+# Input: directory
+# Usage example: ./df.sh freshGenomes
+# Before running: conda activate defense-finder
+
+INPUT_PATH=$1
+
+cd $1
+
+#Loop over all files and run Defense Finder
+for acc_file in *.fa*; do
+	echo "Running defense finder on $acc_file"
+	defense-finder run $acc_file --out-dir dfOutput
+	echo "Ran defense finder on $acc_fasta successfully"
+done
+
