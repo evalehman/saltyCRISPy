8652538fc024895ff22df2338a72f55fc3a070d6
Eva Lehman <evamlehman@gmail.com>
2026-05-04 00:15:58 -0400

    Update meanfinder.sh

--- meanfinder.sh
+++ meanfinder.sh
@@ -1,3 +1,8 @@
+# Takes the mean of all Haloclass salinity protein predictions per proteome 
+# Make sure you're already in the proper directory 
+# Usage example: ./meanfinder.sh
+
+
 awk -F',' 'FNR>1 
 {
     sum[FILENAME]+=$4; count[FILENAME]++
